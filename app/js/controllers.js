0d6385045e5523385d684e96cddb7d14731b0858
W738806 <d.lake.smith@eagles.usm.edu>
2014-10-25 22:28:22 -0500

    Add status message indicator

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,11 +2,43 @@
 
 angular.module('gitwhere.controllers', [])
 .controller('MainController', ['$scope', function($scope) {
+
+	$scope.statusMessage = "All good!";
+
 	var mapOptions = {
           center: { lat: 0, lng: 0},
           zoom: 2
         };
 
+    var marker;
+
 	var map = new google.maps.Map(document.getElementById('map-canvas'),
             mapOptions);
+
+	if (navigator.geolocation) {
+		$scope.statusMessage = "Acquiring location";
+
+		navigator.geolocation.getCurrentPosition(function(position) {
+			var pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+			map.setCenter(pos);
+			map.setZoom(8);
+
+			marker = new google.maps.Marker({
+				position: pos
+			}).setMap(map);
+
+			var onIdleListener = google.maps.event.addListener(map, 'idle', function() {
+				$scope.statusMessage = "All done!";
+				$scope.$apply();
+
+				google.maps.event.removeListener(onIdleListener);
+			});
+		}, function() {
+			$scope.statusMessage = "Can't determine location due to user permissions";
+			$scope.$apply();
+		});
+	} else {
+		$scope.statusMessage = "Can't determine location because the browser doesn't support it";
+	}
+
 }]);
